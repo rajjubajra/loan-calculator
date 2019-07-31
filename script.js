051f6e5aaa3a916eb3fe53376bc631125d048a2a
rajju <vajrachara@yahoo.com>
2019-07-31 04:20:40 +0100

    updated with result

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ document.getElementById('loan-form').addEventListener('submit',calculateResults)
 
 //calculate result
 function calculateResults(e){
- console.log('calculateing...');
+
+console.log('calculateing...');
 
 //UI VARIABLES
 const uiAmount = document.getElementById('amount');
@@ -12,21 +13,54 @@ const uiYears = document.getElementById('years');
 const uiMonlyPayment = document.getElementById('monthly-payment');
 const uiTotalPayment = document.getElementById('total-payment');
 
-const principle = parseFloat(uiAmount.value);
-const calculateInterest = parseFloat(uiInterest.valule)/100/12;
-const calculateYears = parseFloat(uiYears.value)*12;
-
-//compute monthly payment
-const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-const montyly = (principal*x*calculatedInterest)/(x-1)
-
+//loan amount
+const PV = parseFloat(uiAmount.value); 
+//Interest rate
+const R = parseFloat(uiInterest.value / 100 /12);
+//Number of monthly instalments
+const N = uiYears.value * 12;
 
 
+const x = Math.pow(1 + R, N);
+const monthly = (PV * x * R)/(x-1);
+const totalPayment = monthly * N;
+const totalInterest = (monthly * N) - PV;
 
 
+//RESULT UI VARIABLE
+const montly_payment = document.getElementById("monthly-payment");
+const total_payment = document.getElementById("total-payment");
+const total_interest = document.getElementById("total-interest");
 
- e.preventDefault();
+if(isFinite(monthly)){
+  document.querySelector('form').style.display = 'none';
+  document.querySelector('.result').style.display = 'flex';
+  montly_payment.value = monthly.toFixed(2);
+  total_payment.value = totalPayment.toFixed(2);
+  total_interest.value = totalInterest.toFixed(2);
+}else{
+  console.log('no data found');
+  document.querySelector('.error').style.display = 'flex';
+  function clearError(){
+     document.querySelector('.error').style.display = 'none';
+     //console.log('???');
+   }
+  setTimeout( clearError, 3000);
 }
+
+
+
+e.preventDefault();
+
+}
+
+document.addEventListener('click', function(e){
+    if(e.target.id === 'clear'){
+      this.location.reload();
+    }
+})
+
+
 
 
 
